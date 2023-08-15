d023aa527a3b8a00fc58bef146bf42abe7112a6c
Leo <matakleo@gmail.com>
2023-08-15 11:09:19 -0500

    add maxarray

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,7 +3,7 @@ package deque;
 import java.util.Comparator;
 import java.util.Iterator;
 
-public class MaxArrayDeque<T> implements Deque<T>,Iterable<T>  {
+public class MaxArrayDeque<T> extends ArrayDeque<T>  {
     Comparator<T> comparator;
     int size;
     /** new constructor */
@@ -15,63 +15,31 @@ public class MaxArrayDeque<T> implements Deque<T>,Iterable<T>  {
     /** returns the maximum element in the deque as governed by the previously given Comparator.
      * If the MaxArrayDeque is empty, simply return null */
     public T max(){
-        T item;
+
         if (size<1){
             return null;
         }
-        return null;
+        T item = get(0);
+        for (T o : this) {
+           if (comparator.compare(o,item)>0){
+               item = o;
+           }
+        }
+        return item;
     }
     /** returns the maximum element in the deque as governed by the parameter Comparator c.
      *  If the MaxArrayDeque is empty, simply return null. */
     public T max(Comparator<T> c){
-        T item;
         if (size<1){
             return null;
         }
-        return null;
-    }
-    @Override
-    public void addFirst(T item) {
-
-    }
-
-    @Override
-    public void addLast(T item) {
-
+        T item = get(0);
+        for (T o : this){
+            if (c.compare(o,item)>0){
+                item = o;
+            }
+        }
+        return item;
     }
 
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void printDeque() {
-
-    }
-
-    @Override
-    public T removeFirst() {
-        return null;
-    }
-
-    @Override
-    public T removeLast() {
-        return null;
-    }
-
-    @Override
-    public T get(int index) {
-        return null;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return null;
-    }
 }
